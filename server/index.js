adae493a7e80866324168d65723de7552ded6fe1
Jordan Tran <thai.jordan@gmail.com>
2023-03-16 15:37:32 -0700

    added routes and made new files

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from './routes/client.js'
+import managementRoutes from './routes/management.js'
+import generalRoutes from './routes/general.js'
+import salesRoutes from './routes/sales.js'
+
 
 // APP CONFIGS
 dotenv.config();
@@ -18,3 +23,7 @@ app.use(bodyParser.urlencoded({extended: false }));
 app.use(cors());
 
 // ROUTES
+app.use('/client', clientRoutes);
+app.use('/management', managementRoutes);
+app.use('/general', generalRoutes);
+app.use('/sales', salesRoutes);
