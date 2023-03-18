de7a8a461d90122000a42a7cf0127429c4950e10
Jordan Tran <thai.jordan@gmail.com>
2023-03-17 18:12:05 -0700

    cleaned up index.js

--- server/index.js
+++ server/index.js
@@ -15,10 +15,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin'}));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json);
-app.use(bodyParser.urlencoded({extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
