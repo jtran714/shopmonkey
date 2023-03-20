82f8efe2b642d5e12f14345ec983a8d836d4c9f2
Jordan Tran <thai.jordan@gmail.com>
2023-03-20 14:56:12 -0700

    added navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrowDropDownCircleOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
