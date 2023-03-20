2fb20637b834b646fcdbf973ea2569451ff30622
Jordan Tran <thai.jordan@gmail.com>
2023-03-20 15:05:20 -0700

    added appbar from material ui and rendered successfully

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,7 +10,21 @@ import {
 import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
+import { Toolbar, useTheme, AppBar } from "@mui/material";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const dispatch = useDispatch();
+  const theme = useTheme();
+
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}></Toolbar>
+    </AppBar>
+  );
 }
