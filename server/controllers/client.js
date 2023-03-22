c7e0bae047cd3b6ed4faf99186492cac79272c6d
Jordan Tran <thai.jordan@gmail.com>
2023-03-21 21:15:51 -0700

    cleaned up cleintjs

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -37,7 +37,6 @@ export const getCustomers = async (req, res) => {
 
 export const getTransactions = async (req, res) => {
   try {
-    // sort should look like this: { "field": "userId", "sort": "desc"}
     const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
 
     // formatted sort should look like { userId: -1 }
