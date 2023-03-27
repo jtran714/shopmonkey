d9013cace669cf0be1114ee208f3db25f8c0b994
Jordan Tran <thai.jordan@gmail.com>
2023-03-27 12:39:45 -0700

    admin and breakdown done

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,16 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
 import Layout from "scenes/layout";
 import Dashboard from "scenes/dashboard";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+import Geography from "scenes/geography";
+import Overview from "scenes/overview";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
+import Performance from "scenes/performance";
+import Products from "scenes/products";
 
 export default function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -19,6 +29,8 @@ export default function App() {
             <Route element={<Layout />}>
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
